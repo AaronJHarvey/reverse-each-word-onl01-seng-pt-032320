44c7c95a1fe06a7009042a3fab52eb37ee1b479d
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-01 19:48:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,12 @@ sentence1_array.each do |word|
 reversed_array.join(" ") 
 end
 
+def reverse_each_word(sentence1)
+reversed_array=[]
+sentence1_array = sentence1.split(" ")
+sentence1_array.collect do |word|
+  reversed_array << word.reverse
+ 
+ end
+reversed_array.join(" ") 
+end
